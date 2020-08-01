b8183aeed3c12f0508e5f0225a08e839074aa632
Oleg Mamiev <olegios12@gmail.com>
2020-08-01 18:09:23 +0300

    basic authentication

--- app.js
+++ app.js
@@ -37,6 +37,33 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+const auth = (req, res, next) => {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    const err = new Error('You are not authenticated');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const [username, password] = auth;
+
+  if (username === 'admin' && password === 'password') {
+    return next();
+  }
+
+  const err = new Error('You are not authenticated');
+  res.setHeader('WWW-Authenticate', 'Basic');
+  err.status = 401;
+  return next(err);
+};
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
