08ee9f5ec37de0cb3554eeb5285c322deab373be
Oleg Mamiev <olegios12@gmail.com>
2020-08-12 20:05:56 +0300

    https

--- app.js
+++ app.js
@@ -30,6 +30,14 @@ connect
 
 const app = express();
 
+app.all('*', (req, res, next) => {
+  if (req.secure) {
+    return next();
+  }
+
+  res.redirect(307, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
